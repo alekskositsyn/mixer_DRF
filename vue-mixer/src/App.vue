f2e1e98e43547ad9822ede6afab1383feec978e5
alekskositsyn <aleks.kositsyn@gmail.com>
2022-07-02 21:09:45 +0300

    start frontend on vue

---
+++ vue-mixer/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <nav>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="css">
+
+@import "assets/css/bootstrap.css";
+@import "assets/css/fontawesome.css";
+@import "assets/css/style.css";
+</style>
+<!-- vue-mixer/src/assets/css/font-awesome.css -->
