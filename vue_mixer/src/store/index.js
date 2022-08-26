3f079a9b59d50ece9a93bf0b5ca1d7bbcb59d17b
alekskositsyn <aleks.kositsyn@gmail.com>
2022-08-26 12:00:34 +0300

    add settings store

--- vue_mixer/src/store/index.js
+++ vue_mixer/src/store/index.js
@@ -5,13 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: "",
+    isAuthenticated: false,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('token')) {
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      } else {
+        state.token = ""
+        state.isAuthenticated = false
+      }
+    },
+    setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    },
+    removeToken(state, token) {
+      state.token = ""
+      state.isAuthenticated = false
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
