ffe215a22254563d563aac2bf47eaca5f835ba33
alekskositsyn <aleks.kositsyn@gmail.com>
2022-08-26 12:18:29 +0300

    add beforeCreate to App.vue

--- vue_mixer/src/App.vue
+++ vue_mixer/src/App.vue
@@ -4,10 +4,26 @@
       <router-link to="/sign-up">Sign Up</router-link> |
       <router-link to="/log-in">Log In</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+import axios from "axios";
 
+export default {
+  name: 'App',
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    const token = this.$store.state.token
+
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = "Token" + token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
