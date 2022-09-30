fdaab2e0b2f6a0bd9fce5320c931be8e1771b40d
alekskositsyn <aleks.kositsyn@gmail.com>
2022-09-30 21:03:42 +0300

    fix basket items counter

--- vue_mixer/src/store/modules/basket.js
+++ vue_mixer/src/store/modules/basket.js
@@ -1,98 +1,131 @@
+// import { calculateAmount, calculateTotal, copyFunc } from "@/function"
 
 
 // initial state
 // shape: [{ id, quantity }]
 const state = () => ({
-  items: [],
-  countItems: 0,
-  checkoutStatus: null
+    items: [],
+    countItems: 0,
+    checkoutStatus: null
 })
 
 // getters
 const getters = {
-  cartProducts: (state, getters, rootState) => {
-    return state.items.map(({ id, quantity }) => {
-      const product = rootState.products.listProducts.find(product => product.id === id)
-      return {
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        quantity: quantity,
-        image: product.image
-      }
-    })
-  },
+    cartProducts: (state, getters, rootState) => {
+        return state.items.map(({id, quantityBasket}) => {
+            const product = rootState.products.listProducts.find(product => product.id === id)
+            return {
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                quantity: product.quantity,
+                quantityBasket: quantityBasket,
+                image: product.image
+            }
+        })
+    },
 
-  cartTotalPrice: (state, getters) => {
-    return getters.cartProducts.reduce((total, product) => {
-      return total + product.price * product.quantity
-    }, 0)
-  }
+    cartProductsPrice: (state, getters) => {
+        const sumPrice = getters.cartProducts.reduce((acc, product) => {
+            return +product.price * product.quantityBasket
+        }, 0)
+        console.log(sumPrice)
+        // let quantityp = productOne.quantityBasket
+        // console.log(quantityp)
+        return sumPrice
+    },
+
+    cartTotalPrice: (state, getters) => {
+        return getters.cartProducts.reduce((total, product) => {
+            return total + product.price * product.quantityBasket
+        }, 0)
+    }
 }
 
 // actions
 const actions = {
-  checkout ({ commit, state }, products) {
-    const savedCartItems = [...state.items]
-    commit('setCheckoutStatus', null)
-    // empty cart
-    commit('setCartItems', { items: [] })
-    // shop.buyProducts(
-    //   products,
-    //   () => commit('setCheckoutStatus', 'successful'),
-    //   () => {
-    //     commit('setCheckoutStatus', 'failed')
-    //     // rollback to the cart saved before sending the request
-    //     commit('setCartItems', { items: savedCartItems })
-    //   }
-    // )
-  },
+    checkout({commit, state}, products) {
+        const savedCartItems = [...state.items]
+        commit('setCheckoutStatus', null)
+        // empty cart
+        commit('setCartItems', {items: []})
+        // shop.buyProducts(
+        //   products,
+        //   () => commit('setCheckoutStatus', 'successful'),
+        //   () => {
+        //     commit('setCheckoutStatus', 'failed')
+        //     // rollback to the cart saved before sending the request
+        //     commit('setCartItems', { items: savedCartItems })
+        //   }
+        // )
+    },
 
-  addProductToCart ({ state, commit }, product) {
-    commit('setCheckoutStatus', null)
-    if (product.quantity > 0) {
-      commit('incrementCountItem')
-      const cartItem = state.items.find(item => item.id === product.id)
-      if (!cartItem) {
-        commit('pushProductToCart', { id: product.id })
-      } else {
-        commit('incrementItemQuantity', cartItem)
-      }
+    addProductToCart({state, commit}, product) {
+        commit('setCheckoutStatus', null)
+        if (product.quantity > 0) {
+            // commit('incrementCountItem')
+            const cartItem = state.items.find(item => item.id === product.id)
+            if (!cartItem) {
+                commit('pushProductToCart', {id: product.id})
+            } else {
+                commit('incrementItemQuantity', cartItem)
+            }
+        }
+    },
+
+    delProductFromCart({state, commit}, product) {
+        commit('setCheckoutStatus', null)
+        const cartItem = state.items.find(item => item.id === product.id)
+        if (cartItem.quantityBasket > 0) {
+            console.log(cartItem.quantityBasket)
+            commit('decrementItemQuantity', cartItem)
+        }
     }
-  }
 }
 
 // mutations
 const mutations = {
-  pushProductToCart (state, { id }) {
-    state.items.push({
-      id,
-      quantity: 1
-    })
-  },
+    pushProductToCart(state, {id}) {
+        state.items.push({
+            id,
+            quantityBasket: 1
+        })
+        state.countItems++
+    },
 
-  incrementItemQuantity (state, { id }) {
-    const cartItem = state.items.find(item => item.id === id)
-    cartItem.quantity++
-  },
+    incrementItemQuantity(state, {id}) {
+        const cartItem = state.items.find(item => item.id === id)
+        cartItem.quantityBasket++
+        state.countItems++
 
-  incrementCountItem (state) {
-    state.countItems++
-  },
+    },
 
-  setCartItems (state, { items }) {
-    state.items = items
-  },
+    decrementItemQuantity(state, {id}) {
+        const cartItem = state.items.find(item => item.id === id)
+        cartItem.quantityBasket--
+        state.countItems--
+    },
 
-  setCheckoutStatus (state, status) {
-    state.checkoutStatus = status
-  }
+    // incrementCountItem(state) {
+    //     state.countItems++
+    // },
+    // incrementCountItem(state) {
+    //     state.countItems++
+    // },
+
+    setCartItems(state, {items}) {
+        state.items = items
+    },
+
+    setCheckoutStatus(state, status) {
+        state.checkoutStatus = status
+    }
 }
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
 }
--- vue_mixer/src/views/Basket.vue
+++ vue_mixer/src/views/Basket.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="cart">
+  <div class="cart container">
     <h2>Ваша корзина</h2>
-    <div class="left-side">
+    <div class="row">Корзина пуста</div>
+    <div v-show="products.length > 0" class="left-side">
       <div class="cart-product">
-        <div class="row">
-          <div v-for="product in products" :key="product.id" class="col-md-2">
-            <div class="productCard">
-              <div class=" product product-men rounded-3 border shadow">
-                <a href="#" @click="goTo(product.id)" class="linkBuyProduct">
-                  <img :src="product.image" class="img-fluid" alt="">
-                  <div class="nameOfProduct"> {{ product.name }}</div>
-                </a>
-                <div class="numprice">{{ product.price }}
-                  <font-awesome-icon
-                      icon="fa-solid fa-ruble-sign"></font-awesome-icon>
-                </div>
-                <a class="btn btn-danger" href="#" @click="addProductToCart(product)">В корзину</a>
-                <ul class="stars stars-ul">
-                  <li v-for="star in listStar" :key="star">
-                    <a href="#" class="">
-                          <span>
-                              <font-awesome-icon
-                                  :icon="star <= product.middle_rating ? 'fa-solid fa-star' : 'fa-regular fa-star'">
-                              </font-awesome-icon>
-                          </span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+        <table class="table">
+          <caption>Таблица закозов</caption>
+          <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Название</th>
+            <th scope="col">Кол-во</th>
+            <th scope="col">Цена</th>
+            <th scope="col"></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-show="product.quantityBasket >= 0" v-for="product in products" :key="product.id">
+            <td><img :src="product.image" class="img-basket" alt="">
+            </td>
+            <td>{{ product.name }}</td>
+            <td>
+              <el-button @click="addProduct(product)" class="icon-plus" size="mini" round
+                         icon="el-icon-plus"></el-button>
+              {{ product.quantityBasket }}
+              <el-button @click="delProduct(product)" size="mini" round icon="el-icon-minus"></el-button>
+            </td>
+            <td>{{ product.price }}</td>
+            <td>
+              <el-button type="danger" icon="el-icon-delete" circle></el-button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
       </div>
 
     </div>
@@ -46,32 +48,39 @@ import {mapActions, mapGetters, mapState} from 'vuex'
 
 export default {
   name: "Basket",
-    data() {
-    return {
-      popUP: false,
-      listStar: [1, 2, 3, 4, 5]
-    }
-  },
   computed: {
     ...mapState({
       checkoutStatus: state => state.basket.checkoutStatus
     }),
     ...mapGetters('basket', {
       products: 'cartProducts',
-      total: 'cartTotalPrice'
+      total: 'cartTotalPrice',
+      productsPrice: 'cartProductsPrice'
     })
   },
   methods: {
-    ...mapActions('basket', [
-      'addProductToCart'
-    ]),
+    ...mapActions('basket', {
+      addProduct: 'addProductToCart',
+      delProduct: 'delProductFromCart'
+    }),
     checkout(products) {
       this.$store.dispatch('cart/checkout', products)
-    }
+    },
   }
 }
 </script>
 
 <style scoped>
+.cart {
+  margin: auto;
+}
 
+.icon-plus {
+  margin-right: 10px;
+}
+
+.img-basket {
+  height: 45px;
+  width: 45px;
+}
 </style>
